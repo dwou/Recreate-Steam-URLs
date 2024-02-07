5519d0e0889a3fd4f05fd27fe4e1a882f2142e02
dwou <111612803+dwou@users.noreply.github.com>
2024-02-07 16:29:58 -0500

    Add files via upload

--- steamurls.py
+++ steamurls.py
@@ -5,21 +5,20 @@ import re
 # https://steamcommunity.com/profiles/{YOUR_PROFILE_ID}/games?xml=1
 # https://steamcommunity.com/id/{YOUR_PROFILE_ID}/games?xml=1
 
-# Filename should look like this (OLD - games.xml now):
+# Filename should be "games.xml"; formerly:
 # steamcommunity.com_profiles_76561180123456789_games_xml=1.xml
 
-# You'll probably want to index .url files
+# You'll probably want to index .url files; copy to:
+# C:\ProgramData\Microsoft\Windows\Start Menu\Programs
+
 
 filename = r'games.xml'
+illegals = r'/\:*?"<>|' # Windows 10 illegal file name chars
 
-
-illegals = r'/\:*?"<>|'
 
 with open(filename, 'r', encoding='utf-8') as file:
     lines = file.readlines()
-
 lines = [ i[:-1] for i in lines ] # remove newlines
-
 
 # Scan for IDs, create .url file for each
 for i,line in enumerate(lines):
@@ -40,13 +39,14 @@ for i,line in enumerate(lines):
         
         # Write .url file
         with open(filename,'w') as f:
-            print(f'\nname:     {name}')
             print(f'appID:    {ID}')
+            print(f'\nname:     {name}')
             print(f'Filename: {filename}')
             out_lines = [
                 '[InternetShortcut]',
                 'IDList=',
-                'IconIndex=0',
+                'IconFile=C:\Windows\system32\setupapi.dll',
+                'IconIndex=36',
                 f'URL=steam://rungameid/{ID}'
             ]
             for i in out_lines:
